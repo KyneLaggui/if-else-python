a5f566528814e65943c098136bf25926dbcbc2c7
unknown <kynedomereinlaggui@iskolarngbayan.pup.edu.ph>
2021-11-14 17:28:26 +0800

    GDS_PUP_Round_UP_Function

--- Grading_System_Description_PUP.py
+++ Grading_System_Description_PUP.py
@@ -1,48 +1,50 @@
+import math
 Input_Grade_of_User = float(input("Input Grade: "))
+Grade_User_Round_Function = math.ceil(Input_Grade_of_User)
 
-
-def Grade_User_(User_Grade):
-
-    if float(User_Grade) >=97:
+def Grade_User_(Round_User_Grade):
+            
+    if float(Round_User_Grade) >=97:
         print("Your Grade/Mark is 1.00")
         print("You're Excellent")
 
-    elif 94<= float(User_Grade) < 96:
+    elif 94<= float(Round_User_Grade) < 96:
         print("Your Grade/Mark is 1.25")
         print("You're Excellent")
 
-    elif 91<= float(User_Grade) < 93:
+    elif 91<= float(Round_User_Grade) < 93:
         print("Your Grade/Mark is 1.5")
         print("You're Very Good")
     
-    elif 88<= float(User_Grade) < 90:
+    elif 88<= float(Round_User_Grade) < 90:
         print("Your Grade/Mark is 1.75")
         print("You're Very Good")
 
-    elif 83<= float(User_Grade) < 85:
+    elif 85<= float(Round_User_Grade) < 88:
         print("Your Grade/Mark is 2.0")
         print("You're Good")
 
-    elif 82<= float(User_Grade) < 84:
+    elif 82<= float(Round_User_Grade) < 84:
         print("Your Grade/Mark is 2.25")
         print("You're Good")
 
-    elif 79<= float(User_Grade) < 81:
+    elif 79<= float(Round_User_Grade) < 81:
         print("Your Grade/Mark is 2.5")
         print("You're Satisfactory")
 
-    elif 76<= float(User_Grade) < 78:
+    elif 76<= float(Round_User_Grade) < 78:
         print("Your Grade/Mark is 2.75")
         print("You're Satisfactory")
     
-    elif 75== float(User_Grade) :
+    elif 75== float(Round_User_Grade) :
         print("Your Grade/Mark is 3.00")
         print("You're Passing")
 
-    elif 65 <= float(User_Grade) < 74:
+    elif 65 <= float(Round_User_Grade) < 74:
         print("Your Grade/Mark is 5.00")
         print("Sorry you failed")
-    elif 64 <= float(User_Grade):
+
+    elif 64 <= float(Round_User_Grade):
 
         def User_Choices(): 
              print("[1] Incomplete")
@@ -59,4 +61,7 @@ def Grade_User_(User_Grade):
         elif User_Choices_F ==3:
             print("You are dropped, you're no longer part of PUP ")
 
-Grade_User_(Input_Grade_of_User)
+
+
+Grade_User_(Grade_User_Round_Function)
+
